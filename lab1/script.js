3eb7b2304ab0273ed251910c053b91d9c7a20be3
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-01-18 22:18:19 -0500

    Step 3:generated random pet details

--- lab1/script.js
+++ lab1/script.js
@@ -8,3 +8,7 @@ let mood = "";
 // Step2: Declare arrays for random pet data
 const petTypes = ["dog", "cat", "bird", "rabbit"];
 const moods = ["happy", "sleepy", "excited", "hungry"];
+// Step 3:generated random pet details
+petType = petTypes[Math.floor(Math.random() * petTypes.length)];
+petAge = Math.floor(Math.random() * 10);
+mood = moods[Math.floor(Math.random() * moods.length)];

1dadeedc0f1782f1ce4a16aec0782953a5afaa74
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-01-18 22:17:27 -0500

    // Step2: Declare arrays for random pet data

--- lab1/script.js
+++ lab1/script.js
@@ -5,3 +5,6 @@ let petAge = 0;
 let isHungry = false;
 let favoriteActivities = ["sleeping", "playing", "running", "eating"];
 let mood = "";
+// Step2: Declare arrays for random pet data
+const petTypes = ["dog", "cat", "bird", "rabbit"];
+const moods = ["happy", "sleepy", "excited", "hungry"];

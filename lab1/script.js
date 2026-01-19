9e90347c098a0ea6e45cf3642d3a29340f662233
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-01-18 22:14:56 -0500

    step 1 declared and initialized pet behavior in javascript

---
+++ lab1/script.js
@@ -0,0 +1,7 @@
+// Step1: Declare and initialize pet variables
+let petName = "Cherry";
+let petType = "";
+let petAge = 0;
+let isHungry = false;
+let favoriteActivities = ["sleeping", "playing", "running", "eating"];
+let mood = "";

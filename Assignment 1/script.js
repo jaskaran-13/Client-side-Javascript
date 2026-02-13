9a722bd8bba5a93fe6ea3edf17a3400beec33a97
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-02-13 16:48:43 -0500

    step 5 is displayed the full story

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -14,3 +14,32 @@ document.getElementById("part1").addEventListener("click", () => {
     document.getElementById("part1").textContent = selected[0];
     i1 = (i1 + 1) % who.length;
 });
+document.getElementById("part2").addEventListener("click", () => {
+    selected[1] = didWhat[i2];
+    document.getElementById("part2").textContent = selected[1];
+    i2 = (i2 + 1) % didWhat.length;
+});
+
+document.getElementById("part3").addEventListener("click", () => {
+    selected[2] = withWhat[i3];
+    document.getElementById("part3").textContent = selected[2];
+    i3 = (i3 + 1) % withWhat.length;
+});
+
+document.getElementById("part4").addEventListener("click", () => {
+    selected[3] = where[i4];
+    document.getElementById("part4").textContent = selected[3];
+    i4 = (i4 + 1) % where.length;
+});
+
+document.getElementById("part5").addEventListener("click", () => {
+    selected[4] = when[i5];
+    document.getElementById("part5").textContent = selected[4];
+    i5 = (i5 + 1) % when.length;
+});
+
+// step 5 : display the  full story
+document.getElementById("showStory").addEventListener("click", () => {
+    document.getElementById("storyOutput").textContent =
+        selected.join(" ") + ".";
+});

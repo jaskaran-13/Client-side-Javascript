90ff6f86c43c376c73df07d9df095673b0d06bb2
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-02-13 16:44:57 -0500

    step 3 and 4 added , added selected words and button references

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -6,3 +6,11 @@ const where = ["at the park", "in the castle", "on the moon", "at school"];
 const when = ["yesterday", "this morning", "last night", "on Monday"];
 //  step 2 : Added Index trackers
 let i1 = 0, i2 = 0, i3 = 0, i4 = 0, i5 = 0;
+//  step 3 Selected words
+let selected = ["", "", "", "", ""];
+//  step 4 Button references
+document.getElementById("part1").addEventListener("click", () => {
+    selected[0] = who[i1];
+    document.getElementById("part1").textContent = selected[0];
+    i1 = (i1 + 1) % who.length;
+});

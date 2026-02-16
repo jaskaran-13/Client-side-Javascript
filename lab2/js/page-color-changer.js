741ec6b3021b77b6a0c90ccf1810e34c0b73b84c
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-02-15 23:12:21 -0500

    step 1 added selected required html elements by analyzing given html codes added color code sliders and added code to change background color

---
+++ lab2/js/page-color-changer.js
@@ -0,0 +1,15 @@
+/****************************************************
+ * LAB 2 – PAGE COLOR CHANGER
+ ****************************************************/
+/* STEP 1: selected required html elements by analyzing given html codes*/
+// Red color slider
+const redSlider = document.querySelector("#Red");
+
+// Green color slider
+const greenSlider = document.querySelector("#Green");
+
+// Blue color slider
+const blueSlider = document.querySelector("#Blue");
+
+// code to change background color
+const body = document.body;

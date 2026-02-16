c4e73ab75d6f0b712548dd2551662d7ec4b776bf
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-02-15 23:24:25 -0500

    moved files out of folders

--- lab2/page-color-changer.js
+++ lab2/page-color-changer.js
@@ -1,18 +1,21 @@
 /****************************************************
  * LAB 2 – PAGE COLOR CHANGER
  ***/
+document.addEventListener("DOMContentLoaded", function () {
+
 /* STEP 1: selected required html elements by analyzing given html codes*/
 // Red color slider
-const redSlider = document.querySelector("#Red");
+const redSlider = document.querySelector("#red");
 
 // Green color slider
-const greenSlider = document.querySelector("#Green");
+const greenSlider = document.querySelector("#green");
 
 // Blue color slider
-const blueSlider = document.querySelector("#Blue");
+const blueSlider = document.querySelector("#blue");
 
 // code to change background color
 const body = document.body;
+
 /*step2: code added event listner to sliders*/
 // Listener for changes on red slider
 redSlider.addEventListener("input", changeBackgroundColor);
@@ -22,6 +25,7 @@ greenSlider.addEventListener("input", changeBackgroundColor);
 
 // Listener for changes on blue slider
 blueSlider.addEventListener("input", changeBackgroundColor);
+
 //step 3created function to change the backgriund color
 function changeBackgroundColor() {
 
@@ -29,8 +33,12 @@ function changeBackgroundColor() {
     const redValue = redSlider.value;
     const greenValue = greenSlider.value;
     const blueValue = blueSlider.value;
-// STEP 3.2: Created RGB color using slider values
+
+    // STEP 3.2: Created RGB color using slider values
     const rgbColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-// STEP 3.3: Changed the page background color
+
+    // STEP 3.3: Changed the page background color
     body.style.backgroundColor = rgbColor;
 }
+
+});

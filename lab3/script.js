237a198c6a8ddc62644d26de62e65823aab9fdcc
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-16 13:01:20 -0400

    created a javascript constructor

--- lab3/script.js
+++ lab3/script.js
@@ -1,1 +1,21 @@
 //start js code 
+/*
+STEP 1: Created a JavaScript constructor
+
+*/
+
+function ToyCar(name, brand, color, scale, material, price, year, stock, weight, modelNumber, image){
+
+this.name = name
+this.brand = brand
+this.color = color
+this.scale = scale
+this.material = material
+this.price = price
+this.year = year
+this.stock = stock
+this.weight = weight
+this.modelNumber = modelNumber
+this.image = image
+
+}
